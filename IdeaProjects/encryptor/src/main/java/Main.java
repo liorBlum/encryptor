b2959d987ed6b0474367c148ad5043dfcc024aa3
Lior <liorfifa@gmail.com>
2016-05-25 02:14:38 +0300

    finished part 1

--- IdeaProjects/encryptor/src/main/java/Main.java
+++ IdeaProjects/encryptor/src/main/java/Main.java
@@ -3,6 +3,7 @@
  */
 public class Main {
     public static void main(String[] args) {
-
+        Menu menu = Menu.getInstance();
+        menu.showMenu();
     }
 }
--- IdeaProjects/encryptor/src/main/java/Menu.java
+++ IdeaProjects/encryptor/src/main/java/Menu.java
@@ -1,3 +1,6 @@
+
+import java.io.File;
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
 /**
@@ -7,15 +10,49 @@ public class Menu {
     private static Menu instance = null;
     private char encOption = 'e';
     private char decOption = 'd';
-    private String menuText = "For Encryption - enter " + encOption + '\n'
-            + "For Decryption - enter " + decOption;
-    private String errorMsg = "Invalid Input!";
+    private ResourceBundle strings = ResourceBundle.getBundle("strings");
 
     /**
      * Private constructor which avoids instantiation
      */
     private Menu() { }
 
+    /**
+     * Execute encryption/decryption on a given file
+     * @param actionChar action char('e'/'d')
+     * @param file given file
+     */
+    private void executeAction(char actionChar, File file) {
+        if (actionChar == encOption) {
+            System.out.println(strings.getString("encExecMsg")
+                    + " " + file.getName());
+        } else {
+            System.out.println(strings.getString("decExecMsg")
+                    + " " + file.getName());
+        }
+    }
+
+    /**
+     * Get a file path from the user and return the file
+     * which is denoted by that filepath.
+     * @param reader command line input reader
+     * @return file which is denoted by the given filepath
+     */
+    private File getFilePath(Scanner reader) {
+        String filePathString;
+        File file;
+        while (true) {
+            System.out.println(strings.getString("srcPathText"));
+            filePathString = reader.nextLine();
+            file = new File(filePathString);
+            // return a valid file object
+            if (file.canRead()) {
+                return file;
+            } else {
+                System.out.println(strings.getString("inputErrorMsg"));
+            }
+        }
+    }
     /**
      * Get the single Menu object
      * @return Menu object
@@ -26,12 +63,17 @@ public class Menu {
         }
         return instance;
     }
+
+    /**
+     * Show a menu to the user which allows him to choose
+     * between encryption and decryption
+     */
     public void showMenu() {
         Scanner reader = new Scanner(System.in);
         boolean validOutput = false;
         String chosenOption = "";
         while (!validOutput) {
-            System.out.println(menuText);
+            System.out.println(strings.getString("menuText"));
             chosenOption = reader.nextLine();
             /*
             end the loop only when a valid answer is entered
@@ -40,19 +82,13 @@ public class Menu {
                     && (chosenOption.charAt(0) == encOption
                         || chosenOption.charAt(0) == decOption)) {
                 validOutput = true;
+            } else {
+                System.out.println(strings.getString("inputErrorMsg"));
             }
-            else {
-                System.out.println(errorMsg);
-            }
         }
-        //if the user entered 'e', go to Encryption
-        if (chosenOption.charAt(0) == encOption) {
-
-        }
-        // Otherwise, go to Decryption
-        else {
-
-        }
+        //if the user entered 'e', execute Encryption. Otherwise, decryption
+        executeAction(chosenOption.charAt(0), getFilePath(reader));
     }
+
 
 }
